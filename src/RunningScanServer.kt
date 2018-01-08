e477924e2739a1434e74a654ba629409e8058ffb
shakram02 <ahmedhamdyau@gmail.com>
2018-01-08 04:38:37 +0200

    use logger, log connected clients

--- src/RunningScanServer.kt
+++ src/RunningScanServer.kt
@@ -3,10 +3,12 @@ import com.esotericsoftware.kryonet.Listener
 import com.esotericsoftware.kryonet.Server
 import java.util.*
 import java.util.concurrent.LinkedBlockingQueue
+import java.util.logging.Logger
 
 class RunningScanServer(private val messageQueue: LinkedBlockingQueue<String>, tcpPort: Int,
                         udpPort: Optional<Int> = Optional.empty()) {
     private val server = Server()
+    private val logger = Logger.getLogger(this.javaClass.simpleName)
 
     init {
         server.start()
@@ -18,10 +20,15 @@ class RunningScanServer(private val messageQueue: LinkedBlockingQueue<String>, t
         }
 
         server.addListener(object : Listener() {
-            override fun received(clientSocket: Connection?, payload: Any?) {
-                if (payload !is String)
-                    return
+            override fun connected(connection: Connection?) {
+                super.connected(connection)
+                logger.info("A client connected")
+            }
 
+            override fun received(clientSocket: Connection?, payload: Any?) {
+                if (payload !is String) return
+
+                logger.info("Received:" + payload)
                 messageQueue.add(payload)
             }
         })
